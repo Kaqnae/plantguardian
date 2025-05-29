bfe76d0bdcbd213c5681339f96131d29fb491075
Ronnie <148753940+Nessumsa@users.noreply.github.com>
2025-05-29 10:01:15 +0200

    Sign up function

---
+++ lib/features/sign_up/data/auth_service.dart
@@ -0,0 +1,24 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class AuthService {
+  Future<http.Response> signUp({
+    required String name,
+    required String username,
+    required String email,
+    required String password,
+  }) async {
+    final response = await http.post(
+      Uri.parse('${dotenv.env['API_BASE_URL']}/signup'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'name': name,
+        'username': username,
+        'email': email,
+        'password': password,
+      }),
+    );
+    return response;
+  }
+}
--- lib/features/sign_up/presentation/widgets/sign_up_textfields.dart
+++ lib/features/sign_up/presentation/widgets/sign_up_textfields.dart
@@ -3,18 +3,24 @@ import 'package:flutter/material.dart';
 class SignUpTextfields extends StatelessWidget {
   final TextEditingController controller;
   final String label;
+  final String? Function(String?)? validator;
+  final bool obscureText;
 
   const SignUpTextfields({
     super.key,
     required this.controller,
     required this.label,
+    this.validator,
+    this.obscureText = false,
   });
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
       controller: controller,
+      obscureText: obscureText,
       decoration: InputDecoration(labelText: label),
+      validator: validator,
     );
   }
 }
